e624268582ac47d004b45480c7ac36cb58c56268
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2015-10-01 17:44:37 +0200

    switch's --with-exports is a boolean option

--- lib/tacoma/command.rb
+++ lib/tacoma/command.rb
@@ -84,7 +84,7 @@ module Tacoma
     end
     
     desc "switch ENVIRONMENT", "Prepares AWS config files for the providers. --with-exports will output environment variables"
-    option :'with-exports'
+    option :'with-exports', type: :boolean
     
     def switch(environment)
 
